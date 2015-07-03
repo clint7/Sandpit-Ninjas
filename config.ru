06d8819ff631644e02f590cb85cf4881baea1364
Clint <clint@indiereign.com>
2015-07-04 07:43:03 +1200

    added gemfile config.ru and updated main

---
+++ config.ru
@@ -0,0 +1,7 @@
+require 'bundler'
+require 'rubygems'
+
+Bundler.require
+
+require './main'
+run MyApp
--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'json'
 
-get '/' do
- content_type :json
- { :key1 => 'value1', :key2 => 'value2' }.to_json
-end
+class MyApp < Sinatra::Base
+	get '/' do
+	 content_type :json
+	 { :key1 => 'value1', :key2 => 'value2' }.to_json
+	end
+end #end
