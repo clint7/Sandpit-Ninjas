23bbc012eed18d49ffcf35f4e4bddad0dc2aa746
Clint <clint@indiereign.com>
2015-07-04 08:30:57 +1200

    small changes

--- main.rb
+++ main.rb
@@ -2,8 +2,23 @@ require 'sinatra'
 require 'json'
 
 class MyApp < Sinatra::Base
+
 	get '/' do
+	 send_file File.join('public', '/index.html')
+	end
+
+	get '/test.css' do
+	 send_file File.join(settings.public_folder, '/webapp/css/test.css')
+	end
+
+	get '/test_api' do
 	 content_type :json
 	 { :key1 => 'value1', :key2 => 'value2' }.to_json
 	end
+
+	# not_found do
+	#   content_type :json
+	#   halt 404, { error: 'URL not found' }.to_json
+	# end
+
 end #end
