d39558cdd4b0a38196bec916882e4a1d89a750f5
Lorne <thisislorne@gmail.com>
2015-07-04 18:50:25 +1200

    not a clue

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -29,7 +29,34 @@ app.controller('myCtrl', ['$scope', 'dataFactory', function($scope, dataFactory)
   }, function(reason) {
     console.log('Failed: ' + reason);
   });
+
+$scope.icons = ["something", "another", "again"];
+
+
 }]);
+
+
+app.controller('DropdownCtrl', function ($scope, $log) {
+  $scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+});
 
 
 
